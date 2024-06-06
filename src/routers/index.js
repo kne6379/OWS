1d6bb0188330e96296872766b1b8627a4bad409c
blueclorox <dehydrate92@gmail.com>
2024-06-06 21:22:59 +0900

    feed like
    feat: 게시글 좋아요 기능 수정중, schema 일부 변경, router 경로 추가

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { authRouter } from './auth.router.js';
 import { userRouter } from './user.router.js';
 import feedsRouter from './feeds.router.js';
+import { likeRouter } from './like.router.js';
 
 const apiRouter = express.Router();
 
 apiRouter.use('/users', userRouter);
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/feeds', feedsRouter);
+apiRouter.use('/feeds', likeRouter);
 
 export { apiRouter };
---
+++ src/routers/like.router.js
@@ -0,0 +1,71 @@
+import express from 'express'
+import { requireAccessToken } from '../middlewares/require-access-token.middleware.js'
+import { HTTP_STATUS } from '../constants/http-status.constant.js'
+import { MESSAGES } from '../constants/message.constant.js'
+import { prisma } from '../utils/prisma.util.js'
+
+const likeRouter = express.Router()
+
+function likePost(a, b) {
+    const checkData = JSON.parse(a)
+        checkData.includes(b)? checkData.splice(a.indexOf(b),1) : checkData.push(+b)
+        a = JSON.stringify(checkData)
+        return a
+}
+
+likeRouter.post('/:feedId/feedlike', requireAccessToken, async (req, res, next)=> {
+    try {
+        const { userId } = req.user
+        const { feedId } = req.params
+
+        const userData = await prisma.user.findUnique({where: { userId: userId }})
+        const feedData = await prisma.feed.findUnique({where: { feedId: +feedId }})
+        
+
+        const userArr = userData.likedFeedsId
+        const feedArr = feedData.likedUsersId
+
+
+        const changedUser = likePost(userArr, feedId)
+        const changedFeed = likePost(feedArr, userId)
+        
+
+        await prisma.user.update({
+            where: {
+                userId: userId,
+            },
+            data: {
+                likedFeedsId: changedUser,
+            },
+        })
+
+        await prisma.feed.update({
+            where:{
+                feedId: +feedId,
+            },
+            data: {
+                likedUsersId: changedFeed,
+            }
+        })
+        
+        return res.status(HTTP_STATUS.OK).json({
+            message: '처리 되었습니다',
+            data: { userId, feedId }
+          });
+    }catch(error){
+        next(error)
+    }
+})
+
+likeRouter.post('/commentlike', requireAccessToken, async (req, res, next)=>{
+    try {
+        const user = req.user
+
+        const commentId = req.params
+
+    }catch(error){
+        next(error)
+    }
+})
+
+export { likeRouter }
